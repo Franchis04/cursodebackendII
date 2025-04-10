173c017ed3fc41f3beb89efed87547641fdea4c3
fran <francospina2004@gmail.com>
2025-04-09 21:09:08 -0300

    cambios

---
+++ e-commerce/src/schemas/products.schema.js
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+
+export const editProductSchema = {
+  body: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    price: z.number().optional(),
+    thumbnail: z.array().optional(),
+    code: z.string().optional(),
+    stock: z.number().optional(),
+    category: z.string().optional(),
+  }),
+  query: z.object({
+    name: z.string().min(5),
+    price: z.string()
+  })
+};
+
